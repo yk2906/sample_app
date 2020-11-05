e20bac795d958ff0730fc04be863270a747e8c11
Y.Kohama <ec2-user@ip-172-31-3-49.ec2.internal>
2020-11-05 09:14:24 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,11 +10,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
+  # test "should get home" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  #   assert_select "title", "Home | #{@base_title}"
+  # end
 
   test "should get help" do
     get static_pages_help_url
